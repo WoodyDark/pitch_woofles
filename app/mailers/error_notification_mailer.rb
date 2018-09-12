43813a573d327f6d03bb499e67206b7a8a28a138
acjinnn <aliciajinni.ching@gmail.com>
2018-09-12 17:41:01 +0800

    mailer updated

--- app/mailers/error_notification_mailer.rb
+++ app/mailers/error_notification_mailer.rb
@@ -1,6 +1,7 @@
 class ErrorNotificationMailer < ApplicationMailer
 	def notify_owner (feeding)
 		@feeding = feeding
-		mail(to: @feeding.email, subject: "Uh oh! We could not feed your pet...")
+		mail(to: @email.email1, subject: "Uh oh! We could not feed your pet...")
+		mail(to: @email.email2, subject: "Uh oh! We could not feed your pet...")
 	end
 end
--- app/mailers/feeding_notification_mailer.rb
+++ app/mailers/feeding_notification_mailer.rb
@@ -1,6 +1,8 @@
 class FeedingNotificationMailer < ApplicationMailer
 	def notify_owner (feeding)
 		@feeding = feeding
-		mail(to: @feeding.email, subject: "Your pet has been fed!")
+		mail(to: @email.email1, subject: "Your pet has been fed!")
+		mail(to: @email.email2, subject: "Your pet has been fed!")
+		mail(to: @email.email3, subject: "Your pet has been fed!")
 	end
 end
