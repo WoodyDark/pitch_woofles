27b92d2fac66f586096e0a98b6d06d17b76620fa
acjinnn <aliciajinni.ching@gmail.com>
2018-09-12 17:52:33 +0800

    more mailer errors fixed

--- app/mailers/error_notification_mailer.rb
+++ app/mailers/error_notification_mailer.rb
@@ -1,7 +1,7 @@
 class ErrorNotificationMailer < ApplicationMailer
 	def notify_owner (feeding)
 		@feeding = feeding
-		mail(to: @email.email1, subject: "Uh oh! We could not feed your pet...")
-		mail(to: @email.email2, subject: "Uh oh! We could not feed your pet...")
+		mail(to: @feeding.email1, subject: "Uh oh! We could not feed your pet...")
+		mail(to: @feeding.email2, subject: "Uh oh! We could not feed your pet...")
 	end
 end
