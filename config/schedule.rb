c8d3b3def3b92744bee54308b6ca82780e7af2fb
WoodyDark <darkwoodpresents@gmail.com>
2018-09-12 13:39:27 +0800

    Edit now shows current user settings

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
 set :output, "#{path}/log/cron.log"
 
-every 1.day, at: ["9:00 am", "10:00 am", "11:00 am", "12:00 pm"] do
+every 1.day, at: ["7:00 am", "9:00 am", "11:00 am", "1:00 pm"] do
   command "python #{path}/../feed.py"
-  command "echo 'Cron is running now!'"
 end
 
 every :sunday, at: '12pm' do
